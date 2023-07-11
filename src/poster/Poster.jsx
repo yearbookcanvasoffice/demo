ba78fb4c133722f4798f17cf9bd7ee8c682df3fd
yearbookcanvas <mittal.g@yearbookcanvas.com>
2023-07-11 18:38:23 +0530

    30%

---
+++ src/poster/Poster.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {MDBCarousel,MDBCarouselItem} from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
+export default function Poster() {
+  return (
+    <MDBCarousel showControls touch={false}>
+      <MDBCarouselItem
+        className='w-100 d-block'
+        itemId={1}
+        src='https://mdbootstrap.com/img/new/slides/041.jpg'
+        alt='...'
+      />
+      <MDBCarouselItem
+        className='w-100 d-block'
+        itemId={2}
+        src='https://mdbootstrap.com/img/new/slides/042.jpg'
+        alt='...'
+      />
+      <MDBCarouselItem
+        className='w-100 d-block'
+        itemId={3}
+        src='https://mdbootstrap.com/img/new/slides/044.jpg'
+        alt='...'
+      />
+    </MDBCarousel>
+  );
+}
