137248ba103ee7ed8b9d22fdb5ff0d9b7f9526a9
yearbookcanvas <mittal.g@yearbookcanvas.com>
2023-07-15 18:31:46 +0530

    searchbardone

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Start from "./startcompo/Start";
 import Poster from './poster/Poster.jsx'
 import Pos from "./po/Pos";
@@ -6,24 +6,43 @@ import Pos from "./po/Pos";
 
 function App() {
 
-
+  const [start, SetStart] = useState(true);
+  const [dataarr, Setdataarr] = useState([]);
+  const Data=["puma","tiger","lion","car","developer","bold","sex","poco","apple","bolding","bomb"];
+  const InpTag=useRef();
 
   
+   
+   
+
+  // useEffect(() => {
+
+  //   setTimeout(() => {
+  //     SetStart(!start);
+  //   }, 4000)
+  // }, [])
+
+     
+     
 
 
+  function DataSearch(tag)
+  {
+       console.log(tag.value.length);
+    if(tag.value.length!=0)
+    {
+      const NewArray =  Data.filter((e,i,arr)=>{
+        return(e.startsWith(tag.value))
+      })
+      Setdataarr([...NewArray])
 
-
-  const [start, SetStart] = useState(true);
-
-  useEffect(() => {
-
-    setTimeout(() => {
-      SetStart(!start);
-    }, 4000)
-  }, [])
-
-
-
+    }
+    else
+    {
+      Setdataarr([])
+    }
+   
+  }
 
 
 
@@ -31,13 +50,31 @@ function App() {
     <>
       <div className="App">
 
-        {
+        {/* {
           start ? <Start /> : null
-        }
+        } */}
         <div className="com">
           {/* <Poster /> */}
 
-         <Pos/>
+       
+         {/* <Pos/> */}
+         {/* Search  */}
+          
+         <input ref={InpTag} onChange={(e)=>{
+             DataSearch(e.target)
+         }}  type="search" />
+
+         {/* data render */}
+
+         { dataarr.length!=0 ? (<div className="pad12" style={{background:"white",height:"200px",width:"49.99vw",overflowY:"Scroll"}}>
+          {
+            dataarr.map((e,i,arr)=>{
+              return(<div  key={i} onClick={(e)=>{
+                InpTag.current.value=e.target.innerText;
+              }} style={{textAlign:'center',cursor:"pointer"}}>{e}</div>)
+            })
+          }
+         </div>) : null}
 
         </div>
       </div>
---
+++ src/startcompo/Start.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import css from "./start.module.css"
+import cava from './cava.png'
+function Start()
+{
+
+
+
+    
+
+    return(
+        <>
+        <div className={css.main}>
+           {/* <img src={cava} alt="cava" className={css.img} /> */}
+           <h1>YEARBOOK CANVAS</h1>
+           {/* <img src="https://static.vecteezy.com/system/resources/previews/007/559/429/original/wolf-a-wolf-logo-illustration-in-modern-style-free-vector.jpg" alt="logo" /> */}
+        </div>
+        </>
+    )
+}
+
+export default Start;
